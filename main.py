67f15263f0c8173cb1df81d2e30b857157781188
radubizgan <rbizgan7@gmail.com>
2026-06-22 08:34:56 +0300

    Added FPS

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clockObj = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -17,6 +19,7 @@ def main():
              return
         screen.fill("black")
         pygame.display.flip()
+        dt = clockObj.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
