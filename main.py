d3acaa6f2da3c249f3223bde43dcb6539025e968
radubizgan <rbizgan7@gmail.com>
2026-07-13 17:50:12 +0300

    added rotating movement for a and d keys

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
         player.draw(screen)
         pygame.display.flip()
         dt = clockObj.tick(60) / 1000
-
+        player.update(dt)
 
 if __name__ == "__main__":
     main()
